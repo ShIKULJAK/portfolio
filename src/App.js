770f0571fb60a582d260b8d64c2b5476c904868a
ShIKULJA <75026773+ShIKULJAK@users.noreply.github.com>
2022-11-27 12:55:54 +0100

    changed <p> in app

--- src/App.js
+++ src/App.js
@@ -31,7 +31,7 @@ function App() {
           />
           {/* <h4 className="py-10">boris šikuljak</h4> */}
           <p className="py-10">
-            Hello, I'm Boris Šikuljak, and this is the projects from John
+            Hello, I'm Boris Šikuljak, and this are the projects from John
             Smilga's Udemy React course that I'm completed. My github profile:
             <a href="https://github.com/ShIKULJAK" target="_blank">
               github.com/ShIKULJAK
